5f84bd75e91aaa7d457b0de31273a8544912f0f0
D-Tomics <akm001gaming@gmail.com>
2020-03-08 03:32:37 +0530

    v2

--- faultDetectionV1.X/main.c
+++ faultDetectionV1.X/main.c
@@ -1,121 +1,32 @@
-/*
- * File:   main.c
- * Author: hp
- *
- * Created on 24 January, 2020, 2:30 AM
- */
+// PIC16F877A Configuration Bit Settings
+
+// 'C' source line config statements
+
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
 
 #define _XTAL_FREQ 20000000
 
-#define RS RC0
-#define EN RC2
-#define output RD0
-
-#define LCD_DATA PORTB
 #include <xc.h>
-#include <stdint.h>
-
-void ADC_Init() {
-    ADCON0 = 0b01000001;
-    ADCON1 = 0b11000000;
-}
-
-int ADC_Read(uint8_t channel ){
-    ADCON0 &= 0b11000101; // clearing channnel selection bits
-    ADCON0 |= channel << 3; // setting requred bits
-    __delay_ms(1);
-    GO_nDONE = 1;
-    while(GO_nDONE);
-    return ((ADRESH<<8)+ADRESL);
-}
-
-void Lcd_Cmd(char a)
-{
-    RS = 0;           
-    LCD_DATA = a; //Incoming Hex value
-    EN  = 1;         
-    __delay_ms(1);
-    EN  = 0;         
-}
-
-Lcd_Clear()
-{
-    Lcd_Cmd(0); //Clear the LCD
-    Lcd_Cmd(1); //Move the curser to first position
-}
-
-void Lcd_Start()
-{
-  Lcd_Cmd(0x02); //02H is used for Return home -> Clears the RAM and initializes the LCD
-  Lcd_Cmd(0x38);
-  Lcd_Cmd(0x08); //Select Row 1
-  Lcd_Cmd(0x00); //Clear Row 1 Display
-  Lcd_Cmd(0x0C); //Select Row 2
-  Lcd_Cmd(0x00); //Clear Row 2 Display
-  Lcd_Cmd(0x06);
-}
-
-void Lcd_Print_Char(char data)  //Send 8-bits through 4-bit mode
-{
-    RS = 1;
-    LCD_DATA = data;
-    EN = 1;
-    __delay_ms(1);
-    EN = 0;
-}
-
-void Lcd_Print_String(char *a)
-{
-    int i;
-    for(i=0;a[i]!='\0';i++)
-       Lcd_Print_Char(a[i]);  //Split the string using pointers and call the Char function 
-}
-
-void Lcd_Print_Num(int num) {
-    int temp = num;
-    int reverse = 0;
-    for(;temp > 0; ) {
-        int rem = temp%10;
-        temp /= 10;
-        reverse = reverse * 10 + rem;
-    }
-    
-    for(; reverse > 0;) {
-        Lcd_Print_Char(reverse % 10 + 48);
-        reverse /= 10;
-    }
-}
-
-void Lcd_Print_Float(double value) {
-    double integer = (int)value;
-    double decimal = (int)((value - integer) * 100);
-    Lcd_Print_Num((int)integer);
-    Lcd_Print_Char('.');
-    Lcd_Print_Num((int)decimal);
-}
+#include "LCD.h"
+#include "ADC.h"
 
 int main()
 {
-    TRISB = 0x00;
-    TRISC = 0x00;
-    TRISD = 0x00;
-    output = 1;
-    Lcd_Start();
-    ADC_Init();
+    lcd_init();
+    adc_init();             //Initialize the ADC module
+
     while(1)
     {
-        Lcd_Clear();
-        //Lcd_Cmd(0x80);
-        int adc = ADC_Read(4);
-        float input = (adc * (5.0f / 1024.0f)) * 100;
-        
-        if(input > 200 & input < 250)
-            output = 1;
-        else
-            output = 0;
-        Lcd_Print_Float(input);
-    }
-    return 0;
+        int adcValue = adc_read(0);       // Read the ADC value of channel zero
+        lcd_clear();
+        float adc = adcValue * (5.0 / 1024);
+        lcd_float(adc);
+    } 
 }
-
-
